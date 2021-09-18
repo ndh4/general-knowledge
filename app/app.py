4b820bed2ff8d01e6876bbc439cd481b820f0536
ndh4 <ndh4@rice.edu>
2021-09-18 16:30:32 -0500

    debug hopefully -ing

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask, render_template, request, redirect
+from flask_pymongo.wrappers import MongoClient
 from . import settings, controllers, models
 from .extensions import db
 from datetime import datetime
@@ -27,11 +28,17 @@ def create_app(config_object=settings):
 
     app.config['MONGO_URI'] = "mongodb+srv://"+str(user)+":"+str(pwd)+"@cluster0.seola.mongodb.net/"+str(dbname)+"?retryWrites=true&w=majority"
     mongo = PyMongo(app)
+    print("hello")
+    print(mongo.db.sea) ###
+    
+    client = MongoClient(app.config['MONGO_URI'])
+    db = client['knowledge_sea']
+    collection = db['sea']
 
     register_extensions(app)
     register_blueprints(app)
     register_errorhandlers(app)
-    register_routes(app, mongo)
+    register_routes(app, mongo.db.sea)
     return app
 
 def register_extensions(app):
@@ -65,7 +72,7 @@ def register_errorhandlers(app):
 
     return None
 
-def register_routes(app, mongo):
+def register_routes(app, collection):
     @app.route('/')
     def index():
         return render_template('welcome/index.html')
@@ -74,9 +81,18 @@ def register_routes(app, mongo):
     def drop():
         return render_template('welcome/drop.html')
 
-    @app.route('/sea')
-    def sea():
-        return render_template('welcome/sea.html')
+    @app.route('/dive')
+    def begin_dive():
+        if (collection.count() > 0):
+            id = collection.aggregate([{ "$sample": { "size": 1 } }])
+            print(id)
+            return redirect('/dive/' + id)
+        else:
+          return redirect('/empty')
+
+    @app.route('/dive/<string:id>')
+    def view_drop(id):
+        return render_template('welcome/drop.html') # FIXME: add params
 
     @app.route('/stream')
     def stream():
